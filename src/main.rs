e1b21874f110d06801ca7f06cb0c9352ffb73e79
SarahGreyWolf <m.sarahgreywolf@outlook.com>
2021-03-30 16:35:07 +0100

    VGA: Implement custom print and println macros

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,15 @@
 #![no_main]
 
 mod vga;
+mod prelude;
 
 use core::panic::PanicInfo;
 use core::fmt::Write;
-use vga::{Writer, Colour};
+use prelude::*;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let mut vga_buffer = vga::Writer::init();
-
-    vga_buffer.write_string("This is a test\n", Colour::Grey,
-                            Colour::LightMagenta);
-    write!(vga_buffer, "{:#}\n", 120 - (1*80));
-    vga_buffer.write_string("This is a test\n", Colour::Yellow,
-                            Colour::LightMagenta);
-    write!(vga_buffer, "{:#}\n", 170 / 80);
+    println!("Test");
     loop {}
 }
 
---
+++ src/prelude.rs
@@ -0,0 +1,1 @@
+pub use crate::vga::{VGA_WRITER};
