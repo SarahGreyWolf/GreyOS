3cf751055e52e5fb084653c46f74ed1eda11d25a
SarahGreyWolf <m.sarahgreywolf@outlook.com>
2021-03-30 04:07:04 +0100

    Abandon first edition because it's giving me a headache

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(asm)]
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+
+// Called on Panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    // If it panics, loop forever
+    // Like executing a nop forever?
+    loop{}
 }
