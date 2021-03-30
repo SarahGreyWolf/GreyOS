68cde8ffa7c85f965249e80950ae361b5202c717
SarahGreyWolf <m.sarahgreywolf@outlook.com>
2021-03-30 05:14:34 +0100

    Basic Kernel: Write to VGA Text Buffer to screen

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,34 @@
 
 use core::panic::PanicInfo;
 
+// Bytes of ascii Hello World to write to VGA Text Buffer
+static HELLO: &[u8] = b"Hello World";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+
+    // Create a pointer to the memory address of the VGA Buffer
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            // Write to the memory address of the buffer with an offset
+            // EG. Write the ascii value of e to 0xb8000 + 1
+            *vga_buffer.offset(i as isize * 2) = byte;
+            // Make the foreground/text colour a light cyan
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x9 | 0x40;
+        }
+    }
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            // Write to the memory address of the buffer with an offset
+            // EG. Write the ascii value of e to 0xb8000 + 1
+            *vga_buffer.offset(i as isize * 2 + 160) = byte;
+            // Make the background colour a light cyan
+            *vga_buffer.offset(i as isize * 2 + 1 + 160) = 0xb;
+        }
+    }
+
     loop {}
 }
 
